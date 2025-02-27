d63595d46fbc5649804927c5734dd26d49cb7292
Dominik Loidolt <dominik.loidolt@univie.ac.at>
2025-02-27 19:46:06 +0100

    test(cli): add CLI tests
    - Added `test/cli_basic_test.py` with basic tests for CLI options, including version, help text, and invalid options.
    - Added `test/cli_compression_test.py` with tests for CLI compression options.
    - Created `test/clitest.py` a CLI testing framework.
    - Updated `test/meson.build` to include CLI tests using the new framework.

---
+++ test/cli_basic_test.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""
+@file   cli_basic_test.py
+@author Dominik Loidolt (dominik.loidolt@univie.ac.at)
+@date   2025
+
+@copyright GPLv2
+This program is free software; you can redistribute it and/or modify it
+under the terms and conditions of the GNU General Public License,
+version 2, as published by the Free Software Foundation.
+
+This program is distributed in the hope it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+more details.
+
+@brief AIRSPACE CLI Basic Option Tests
+"""
+
+import unittest
+from pathlib import Path
+
+import clitest
+from clitest import RETURN_FAILURE, CliTest, assertCli
+from get_library_version import extract_version
+
+CMP_H_PATH = Path(__file__).resolve().parent.joinpath("../lib/cmp.h")
+VERSION_STR_EXP = extract_version(CMP_H_PATH)
+
+
+class TestBasicOptions(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.cli_test = CliTest()
+        cls.airspace = cls.cli_test.run_cli
+
+    def test_print_version_string_to_stdout(self):
+        for arg in [["-V"], ["--version"], ["-qvV"]]:
+            with self.subTest(arg=arg):
+
+                result = self.airspace(arg)
+
+                self.assertCli(
+                    result, stdout_exp=VERSION_STR_EXP, stdout_match_mode="contains"
+                )
+
+    def test_print_version_minimal_string_to_stdout(self):
+        for arg in [["-qV"], ["--quiet", "--version"], ["-q", "-V"]]:
+            with self.subTest(arg=arg):
+
+                result = self.airspace(arg)
+
+                self.assertCli(result, stdout_exp=VERSION_STR_EXP + "\n")
+
+    def test_print_help_text(self):
+        for arg in [["-h"], ["--help"]]:
+            with self.subTest(arg=arg):
+
+                result = self.airspace(arg)
+
+                self.assertCli(
+                    result, stdout_exp="Usage: ", stdout_match_mode="contains"
+                )
+
+    def test_invalid_long_options_are_detected(self):
+        result = self.airspace("--my_invalid_option")
+
+        self.assertCli(
+            result,
+            returncode_exp=RETURN_FAILURE,
+            stderr_exp="my_invalid_option",
+            stderr_match_mode="contains",
+        )
+
+    def test_invalid_short_options_are_detected(self):
+        result = self.airspace("-u")
+
+        self.assertCli(
+            result,
+            returncode_exp=RETURN_FAILURE,
+            stderr_exp="u",
+            stderr_match_mode="contains",
+        )
+
+    def test_o_option_needs_an_argument(self):
+        result = self.airspace(["-c", "-o"], stdin=bytes.fromhex("0001 0002"))
+
+        self.assertCli(
+            result,
+            returncode_exp=RETURN_FAILURE,
+            stderr_exp="requires an argument",
+            stderr_match_mode="contains",
+        )
+
+
+if __name__ == "__main__":
+    clitest.main()
