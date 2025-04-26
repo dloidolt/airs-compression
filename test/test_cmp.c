7c4555c28bbccae259df11a3f941aff1025bab0d
Dominik Loidolt <dominik.loidolt@univie.ac.at>
2025-04-26 19:09:03 +0200

    refactor: Move uncompressed context creation to helper function
    This commit refactors the test code to use a dedicated helper function,
    `create_uncompressed_context`, for creating and initializing the
    `cmp_context` in uncompressed mode.
    The global `g_ctx_uncompressed`, `g_src`, and `g_dst` variables have
    been removed, and each test case now obtains its own local variables
    as needed.

--- test/test_cmp.c
+++ test/test_cmp.c
@@ -18,25 +18,19 @@
 #include "../lib/common/header.h"
 
 
-/* Global variables */
-static struct cmp_context g_ctx_uncompressed;
-static const uint16_t g_src[2] = { 0x0001, 0x0203 };
-static uint8_t g_dst[CMP_HDR_SIZE + sizeof(g_src)];
-
-
-/* Test setup executed for every test case */
-void setUp(void)
+static struct cmp_context create_uncompressed_context(void)
 {
+	struct cmp_context ctx;
 	struct cmp_params par_uncompressed = { 0 };
 	uint32_t return_val;
 
 	par_uncompressed.mode = CMP_MODE_UNCOMPRESSED;
 	par_uncompressed.primary_preprocessing = CMP_PREPROCESS_NONE;
 	/* we do not need a working buffer for CMP_MODE_UNCOMPRESSED */
-	return_val = cmp_initialise(&g_ctx_uncompressed, &par_uncompressed, NULL, 0);
+	return_val = cmp_initialise(&ctx, &par_uncompressed, NULL, 0);
 	TEST_ASSERT_CMP_SUCCESS(return_val);
 
-	memset(g_dst, 0xBE, sizeof(g_dst));
+	return ctx;
 }
 
 
@@ -195,10 +189,11 @@ void test_compression_in_uncompressed_mode(void)
 	uint8_t dst[CMP_HDR_SIZE + sizeof(data)];
 	/* uncompressed data should be in big endian */
 	const uint8_t cmp_data_exp[sizeof(data)] = { 0x00, 0x01, 0x02, 0x03 };
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
 	struct cmp_hdr hdr;
 
 	uint32_t const cmp_size =
-		cmp_compress_u16(&g_ctx_uncompressed, dst, sizeof(dst), data, sizeof(data));
+		cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), data, sizeof(data));
 
 	TEST_ASSERT_CMP_SUCCESS(cmp_size);
 	TEST_ASSERT_EQUAL(CMP_HDR_SIZE + 4, cmp_size);
@@ -214,10 +209,12 @@ void test_compression_in_uncompressed_mode(void)
 
 void test_compression_detects_too_small_dst_buffer(void)
 {
-	uint8_t dst[CMP_HDR_SIZE + sizeof(g_src) - 1];
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+	const uint16_t src[2] = { 0x0001, 0x0203 };
+	uint8_t dst[CMP_HDR_SIZE + sizeof(src) - 1];
 
 	uint32_t const cmp_size =
-		cmp_compress_u16(&g_ctx_uncompressed, dst, sizeof(dst), g_src, sizeof(g_src));
+		cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), src, sizeof(src));
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_DST_TOO_SMALL, cmp_size);
 }
@@ -225,8 +222,10 @@ void test_compression_detects_too_small_dst_buffer(void)
 
 void test_compression_detects_missing_context(void)
 {
-	uint32_t const cmp_size =
-		cmp_compress_u16(NULL, g_dst, sizeof(g_dst), g_src, sizeof(g_src));
+	const uint16_t src[2] = { 0x0001, 0x0203 };
+	uint8_t dst[CMP_HDR_SIZE + sizeof(src)];
+
+	uint32_t const cmp_size = cmp_compress_u16(NULL, dst, sizeof(dst), src, sizeof(src));
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_CONTEXT_INVALID, cmp_size);
 }
@@ -234,8 +233,10 @@ void test_compression_detects_missing_context(void)
 
 void test_compression_detects_missing_dst_buffer(void)
 {
-	uint32_t const size =
-		cmp_compress_u16(&g_ctx_uncompressed, NULL, 0, g_src, sizeof(g_src));
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+	const uint16_t src[2] = { 0x0001, 0x0203 };
+
+	uint32_t const size = cmp_compress_u16(&ctx_uncompressed, NULL, 0, src, sizeof(src));
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_DST_NULL, size);
 }
@@ -243,8 +244,11 @@ void test_compression_detects_missing_dst_buffer(void)
 
 void test_compression_detects_missing_src_data(void)
 {
-	uint32_t const size = cmp_compress_u16(&g_ctx_uncompressed, g_dst, sizeof(g_dst),
-					       NULL, sizeof(uint16_t));
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+	uint8_t dst[CMP_HDR_SIZE + sizeof(uint16_t)];
+
+	uint32_t const size =
+		cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), NULL, sizeof(uint16_t));
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_SRC_NULL, size);
 }
@@ -252,8 +256,11 @@ void test_compression_detects_missing_src_data(void)
 
 void test_compression_detects_src_size_is_0(void)
 {
-	uint32_t const cmp_size =
-		cmp_compress_u16(&g_ctx_uncompressed, g_dst, sizeof(g_dst), g_src, 0);
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+	const uint16_t src[2] = { 0x0001, 0x0203 };
+	uint8_t dst[CMP_HDR_SIZE + sizeof(src)];
+
+	uint32_t const cmp_size = cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), src, 0);
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_SRC_SIZE_WRONG, cmp_size);
 }
@@ -261,11 +268,13 @@ void test_compression_detects_src_size_is_0(void)
 
 void test_compression_detects_src_size_is_not_multiple_of_2(void)
 {
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
 	const uint16_t src[2] = { 0 };
 	uint32_t const src_size = 3;
+	uint8_t dst[CMP_HDR_SIZE + sizeof(src)];
 
 	uint32_t const cmp_size =
-		cmp_compress_u16(&g_ctx_uncompressed, g_dst, sizeof(g_dst), src, src_size);
+		cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), src, src_size);
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_SRC_SIZE_WRONG, cmp_size);
 }
@@ -273,11 +282,13 @@ void test_compression_detects_src_size_is_not_multiple_of_2(void)
 
 void test_compression_detects_src_size_too_large(void)
 {
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+	uint8_t dst[CMP_HDR_SIZE + sizeof(uint16_t)];
+	const uint16_t src[2] = { 0x0001, 0x0203 };
 	uint32_t const src_size_too_large = CMP_MAX_ORIGINAL_SIZE + 1;
 
-	uint32_t const cmp_size
-		= cmp_compress_u16(&g_ctx_uncompressed, g_dst, sizeof(g_dst),
-				   g_src, src_size_too_large);
+	uint32_t const cmp_size =
+		cmp_compress_u16(&ctx_uncompressed, dst, sizeof(dst), src, src_size_too_large);
 
 	TEST_ASSERT_EQUAL_CMP_ERROR(CMP_ERR_SRC_SIZE_WRONG, cmp_size);
 }
@@ -285,7 +296,9 @@ void test_compression_detects_src_size_too_large(void)
 
 void test_successful_reset_of_compressed_data(void)
 {
-	uint32_t const return_val = cmp_reset(&g_ctx_uncompressed);
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
+
+	uint32_t const return_val = cmp_reset(&ctx_uncompressed);
 
 	TEST_ASSERT_CMP_SUCCESS(return_val);
 }
@@ -301,11 +314,12 @@ void test_compression_reset_detect_missing_context(void)
 
 void test_deinitialise_a_compression_context(void)
 {
+	struct cmp_context ctx_uncompressed = create_uncompressed_context();
 	struct cmp_context const zero_ctx = { 0 };
 
-	cmp_deinitialise(&g_ctx_uncompressed);
+	cmp_deinitialise(&ctx_uncompressed);
 
-	TEST_ASSERT_EQUAL_MEMORY(&g_ctx_uncompressed, &zero_ctx, sizeof(g_ctx_uncompressed));
+	TEST_ASSERT_EQUAL_MEMORY(&ctx_uncompressed, &zero_ctx, sizeof(ctx_uncompressed));
 }
 
 
@@ -344,8 +358,8 @@ void test_detect_0_size_work_buffer(void)
 void test_compression_detects_too_small_work_buffer(void)
 {
 	struct cmp_params params = { 0 };
-	const uint16_t data[] = { 0, 0, 0};
-	uint8_t work_buf[sizeof(data)-1];
+	const uint16_t data[] = { 0, 0, 0 };
+	uint8_t work_buf[sizeof(data) - 1];
 	struct cmp_context ctx;
 	uint32_t dst_size, work_buf_size;
 	uint8_t dst[CMP_HDR_SIZE + sizeof(data)];
