549164842da42d6333df54d13daaa4a723794fcd
Dominik Loidolt <dominik.loidolt@univie.ac.at>
2025-05-13 18:34:53 +0200

    refactor(test): move common test utilities to separate compilation unit
    - Extracted test_common.c from test_common.h for better modularity
    - Created a static library for shared test utilities
    - Updated Meson build to link unit tests with test_common_lib

---
+++ test/test_common.c
@@ -0,0 +1,106 @@
+/**
+ * @file
+ * @author Dominik Loidolt (dominik.loidolt@univie.ac.at)
+ * @date   2025
+ * @copyright GPL-2.0
+ *
+ * @brief Utilities for testing the compression library
+ */
+
+#include <stdio.h>
+
+#include <unity.h>
+#include <unity_internals.h>
+
+
+#include "../lib/cmp_errors.h"
+
+
+/**
+ * @brief Converts compression error enum to string
+ *
+ * @param error		Compression error code
+ *
+ * @returns error code string
+ */
+
+static const char *cmp_error_enum_to_str(enum cmp_error error)
+{
+	switch (error) {
+	case CMP_ERR_NO_ERROR:
+		return "CMP_ERR_NO_ERROR";
+	case CMP_ERR_GENERIC:
+		return "CMP_ERR_GENERIC";
+	case CMP_ERR_PARAMS_INVALID:
+		return "CMP_ERR_PARAMS_INVALID";
+	case CMP_ERR_CONTEXT_INVALID:
+		return "CMP_ERR_CONTEXT_INVALID";
+	case CMP_ERR_WORK_BUF_NULL:
+		return "CMP_ERR_WORK_BUF_NULL";
+	case CMP_ERR_WORK_BUF_TOO_SMALL:
+		return "CMP_ERR_WORK_BUF_TOO_SMALL";
+	case CMP_ERR_DST_NULL:
+		return "CMP_ERR_DST_NULL";
+	case CMP_ERR_DST_UNALIGNED:
+		return "CMP_ERR_DST_UNALIGNED";
+	case CMP_ERR_SRC_NULL:
+		return "CMP_ERR_SRC_NULL";
+	case CMP_ERR_SRC_SIZE_WRONG:
+		return "CMP_ERR_SRC_SIZE_WRONG";
+	case CMP_ERR_DST_TOO_SMALL:
+		return "CMP_ERR_DST_TOO_SMALL";
+	case CMP_ERR_SRC_SIZE_MISMATCH:
+		return "CMP_ERR_SRC_SIZE_MISMATCH";
+	case CMP_ERR_TIMESTAMP_INVALID:
+		return "CMP_ERR_TIMESTAMP_INVALID";
+	case CMP_ERR_INT_HDR:
+		return "CMP_ERR_INT_HDR";
+	case CMP_ERR_INT_ENCODER:
+		return "CMP_ERR_INT_ENCODER";
+	case CMP_ERR_INT_BITSTREAM:
+		return "CMP_ERR_INT_BITSTREAM";
+	case CMP_ERR_MAX_CODE:
+	default:
+		TEST_FAIL_MESSAGE("Missing error name");
+		return "Unknown error";
+	}
+}
+
+
+/**
+ * @brief Generates a descriptive error message
+ *
+ * @param expected	expected error code
+ * @param actual	actual error code
+ *
+ * @returns formatted error message string
+ */
+
+static const char *gen_cmp_error_message(enum cmp_error expected,
+					 enum cmp_error actual)
+{
+	enum{ CMP_TEST_MESSAGE_BUF_SIZE = 128 };
+	static char message[CMP_TEST_MESSAGE_BUF_SIZE];
+
+	snprintf(message, sizeof(message), "Expected %s Was %s.",
+		 cmp_error_enum_to_str(expected), cmp_error_enum_to_str(actual));
+	return message;
+}
+
+
+/**
+ * @brief Asserts compression error code equality
+ *
+ * @param expected_error	expected error code
+ * @param cmp_ret_code		compression library return code
+ */
+
+void assert_equal_cmp_error_internal(enum cmp_error expected_error,
+				     uint32_t cmp_ret_code, int line)
+{
+	enum cmp_error actual_error = cmp_get_error_code(cmp_ret_code);
+	const char *message = gen_cmp_error_message(expected_error, actual_error);
+
+	UNITY_TEST_ASSERT_EQUAL_INT(expected_error, actual_error, line,
+				    message);
+}
--- test/test_common.h
+++ test/test_common.h
@@ -7,14 +7,12 @@
  * @brief Utilities for testing the compression library
  */
 
-#include <stdio.h>
 
 #include <unity.h>
-#include <unity_internals.h>
-
-
 #include "../lib/cmp_errors.h"
 
+void assert_equal_cmp_error_internal(enum cmp_error expected_error,
+				     uint32_t cmp_ret_code, int line);
 
 
 /**
@@ -37,76 +35,6 @@
 #define TEST_ASSERT_CMP_FAILURE(cmp_ret_code) TEST_ASSERT_TRUE(cmp_is_error(cmp_ret_code))
 
 
-/**
- * @brief Converts compression error enum to string
- *
- * @param error		Compression error code
- *
- * @returns error code string
- */
-
-static __inline const char *cmp_error_enum_to_str(enum cmp_error error)
-{
-	switch (error) {
-	case CMP_ERR_NO_ERROR:
-		return "CMP_ERR_NO_ERROR";
-	case CMP_ERR_GENERIC:
-		return "CMP_ERR_GENERIC";
-	case CMP_ERR_PARAMS_INVALID:
-		return "CMP_ERR_PARAMS_INVALID";
-	case CMP_ERR_CONTEXT_INVALID:
-		return "CMP_ERR_CONTEXT_INVALID";
-	case CMP_ERR_WORK_BUF_NULL:
-		return "CMP_ERR_WORK_BUF_NULL";
-	case CMP_ERR_WORK_BUF_TOO_SMALL:
-		return "CMP_ERR_WORK_BUF_TOO_SMALL";
-	case CMP_ERR_DST_NULL:
-		return "CMP_ERR_DST_NULL";
-	case CMP_ERR_DST_UNALIGNED:
-		return "CMP_ERR_DST_UNALIGNED";
-	case CMP_ERR_SRC_NULL:
-		return "CMP_ERR_SRC_NULL";
-	case CMP_ERR_SRC_SIZE_WRONG:
-		return "CMP_ERR_SRC_SIZE_WRONG";
-	case CMP_ERR_DST_TOO_SMALL:
-		return "CMP_ERR_DST_TOO_SMALL";
-	case CMP_ERR_SRC_SIZE_MISMATCH:
-		return "CMP_ERR_SRC_SIZE_MISMATCH";
-	case CMP_ERR_TIMESTAMP_INVALID:
-		return "CMP_ERR_TIMESTAMP_INVALID";
-	case CMP_ERR_INT_HDR:
-		return "CMP_ERR_INT_HDR";
-	case CMP_ERR_INT_ENCODER:
-		return "CMP_ERR_INT_ENCODER";
-	case CMP_ERR_INT_BITSTREAM:
-		return "CMP_ERR_INT_BITSTREAM";
-	case CMP_ERR_MAX_CODE:
-	default:
-		TEST_FAIL_MESSAGE("Missing error name");
-		return "Unknown error";
-	}
-}
-
-
-/**
- * @brief Generates a descriptive error message
- *
- * @param expected	expected error code
- * @param actual	actual error code
- *
- * @returns formatted error message string
- */
-
-static __inline const char *gen_cmp_error_message(enum cmp_error expected,
-						  enum cmp_error actual)
-{
-	enum{ CMP_TEST_MESSAGE_BUF_SIZE = 128 };
-	static char message[CMP_TEST_MESSAGE_BUF_SIZE];
-
-	snprintf(message, sizeof(message), "Expected %s Was %s.",
-		 cmp_error_enum_to_str(expected), cmp_error_enum_to_str(actual));
-	return message;
-}
 
 
 /**
@@ -117,16 +45,7 @@ static __inline const char *gen_cmp_error_message(enum cmp_error expected,
  */
 
 #define TEST_ASSERT_EQUAL_CMP_ERROR(expected_CMP_ERROR, cmp_ret_code) \
-  assert_equal_cmp_error_internal(expected_CMP_ERROR, cmp_ret_code, __LINE__)
-static __inline void assert_equal_cmp_error_internal(enum cmp_error expected_error,
-					    uint32_t cmp_ret_code, int line)
-{
-	enum cmp_error actual_error = cmp_get_error_code(cmp_ret_code);
-	const char* message = gen_cmp_error_message(expected_error, actual_error);
-
-	UNITY_TEST_ASSERT_EQUAL_INT(expected_error, actual_error, line,
-				    message);
-}
+	assert_equal_cmp_error_internal(expected_CMP_ERROR, cmp_ret_code, __LINE__)
 
 
 /**
